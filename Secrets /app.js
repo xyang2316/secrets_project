5ab637b738726411ed8b994793107ca54b497b39
yxy1123 <31700103+yxy1123@users.noreply.github.com>
2020-09-13 16:17:20 -0700

    upload secret folder

---
+++ Secrets /app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});//jshint esversion:6
